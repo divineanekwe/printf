f923c12bc38dfddc81c317f2a7fb101ad5b06651
Peaceezike <peaceezike50@gmail.com>
2022-10-17 16:05:05 +0000

    Create prototype: get_flag

--- main.h
+++ main.h
@@ -10,4 +10,20 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _putstr(char *str);
 
+/**
+  * struct flags - activates specified flag(s) to _printf()
+  * @plus: '+' flag
+  * @space: ' ' flag
+  * @hash: '#' flag
+  */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/* get functions */
+int get_flag(char s, flags_t *f);
+
 #endif /* MAIN_H */
