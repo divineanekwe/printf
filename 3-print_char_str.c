7b3ba673d245a7f79580ed1d5058816a4fe1513d
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-18 15:43:04 +0000

    Fix logic flow

--- 3-print_char_str.c
+++ 3-print_char_str.c
@@ -6,7 +6,7 @@
   *
   * Return: Number of characters printed
   */
-int print_s(va_list arg)
+int print_s(va_list arg, __attribute__((unused)) flags_t *flag)
 {
 	char *str = va_arg(arg, char *);
 
@@ -21,8 +21,8 @@ int print_s(va_list arg)
   *
   * Return: Always 1
   */
-int print_c(va_list arg)
+int print_c(va_list arg, __attribute__((unused)) flags_t *flag)
 {
-	_putchar(va_arg(1, int));
+	_putchar(va_arg(arg, int));
 	return (1);
 }
