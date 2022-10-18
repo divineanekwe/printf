e8949be347357525d0a77218b28054742174c8ac
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-18 12:50:55 +0000

    Add function prototype: print_p

--- main.h
+++ main.h
@@ -15,6 +15,7 @@ int print_S(va_list arg);
 int print_r(va_list arg);
 int print_R(va_list arg);
 int print_percent(void);
+int print_p(va_list arg);
 
 /**
   * struct flags - activates specified flag(s) to _printf()
