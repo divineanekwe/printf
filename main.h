aabbd21077d4eb67c50970b7728b81428df9274e
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-18 15:42:21 +0000

    Correct function declaration for print prototypes

--- main.h
+++ main.h
@@ -5,20 +5,8 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-/** print prototypes */
-int _printf(const char *format, ...);
-int _putchar(char c);
-int _putstr(char *str);
-int print_s(va_list arg);
-int print_c(va_list arg);
-int print_S(va_list arg);
-int print_r(va_list arg);
-int print_R(va_list arg);
-int print_percent(void);
-int print_p(va_list arg);
-
 /**
-  * struct flags - activates specified flag(s) to _printf()
+  * struct flags - signifies specified flags(s) to _printf
   * @plus: '+' flag
   * @space: ' ' flag
   * @hash: '#' flag
@@ -30,9 +18,21 @@ typedef struct flags
 	int hash;
 } flags_t;
 
+/** print prototypes */
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _putstr(char *str);
+int print_s(va_list arg, flags_t *flag);
+int print_c(va_list arg, flags_t *flag);
+int print_S(va_list arg, flags_t *flag);
+int print_r(va_list arg, flags_t *flag);
+int print_R(va_list arg, flags_t *flag);
+int print_percent(va_list arg, flags_t *flag);
+int print_p(va_list arg, flags_t *flag);
+
 /* get prototypes */
-int get_flag(char c, flags_t *f);
-int (*get_print(char c))(va_list, flags_t *f);
+int get_flag(char c, flags_t *flag);
+int (*get_print(char c))(va_list, flags_t *);
 
 /**
   * struct print_help - chooses the correct function based on
@@ -43,18 +43,18 @@ int (*get_print(char c))(va_list, flags_t *f);
 typedef struct print_help
 {
 	char c;
-	int (*f)(va_list arg, flags_t *f);
+	int (*f)(va_list arg, flags_t *flag);
 } print_help_t;
 
 /* print number(s) prototypes */
 int int_count(int n);
 void print_num(int n);
-int print_i(va_list arg, flags_t *f);
+int print_i(va_list arg, flags_t *flag);
 char *num_to_str(unsigned long int num, int base, int type);
-int print_u(va_list arg);
-int print_x(va_list arg, flags_t *f);
-int print_X(va_list arg, flags_t *f);
-int print_b(va_list arg);
-int print_o(va_list arg, flags_t *f);
+int print_u(va_list arg, flags_t *flag);
+int print_x(va_list arg, flags_t *flag);
+int print_X(va_list arg, flags_t *flag);
+int print_b(va_list arg, flags_t *flag);
+int print_o(va_list arg, flags_t *flag);
 
 #endif /* MAIN_H */
